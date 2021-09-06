358fa445b0e512c72c0d68d2074bdf5d7b36d588
Carlos Urquiza <webquiza@gmail.com>
2021-09-06 16:49:33 -0700

    add initial js logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
+// scope everything inside the game function 
 
+const game = () => {
+
+  let pScore = 0;
+  let cScore = 0;
+
+  //start the game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  //play match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+
+    //computer options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //computer choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+     
+      });
+    });
+  };
+
+  //calls the startGame function
+  startGame();
+  playMatch();
+};
+
+//calls the game function
+game();
